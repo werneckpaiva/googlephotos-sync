98daf87d9bd7bd59a4f5b9b9d33bc6876812c99b
Ricardo Paiva <werneck.paiva@gmail.com>
2026-01-11 15:24:44 +0000

    fixes exception

--- src/main/java/com/werneckpaiva/googlephotosbatch/exception/PermissionDeniedToLoadAlbumsException.java
+++ src/main/java/com/werneckpaiva/googlephotosbatch/exception/PermissionDeniedToLoadAlbumsException.java
@@ -1,7 +1,6 @@
 package com.werneckpaiva.googlephotosbatch.exception;
 
-
-public class PermissionDeniedToLoadAlbumsException extends Throwable {
+public class PermissionDeniedToLoadAlbumsException extends Exception {
     public PermissionDeniedToLoadAlbumsException(RuntimeException e) {
         super(e);
     }
